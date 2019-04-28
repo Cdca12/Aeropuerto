e79c1594d52eb8d82eef77cb56871ced8decca69
Carlos Contreras <cdca12mx@gmail.com>
2019-04-28 16:10:08 -0600

    Creación de proyecto

---
+++ src/aeropuerto/Aeropuerto.java
@@ -0,0 +1,13 @@
+package aeropuerto;
+
+/**
+ *
+ * @author Carlos Contreras
+ */
+public class Aeropuerto {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
