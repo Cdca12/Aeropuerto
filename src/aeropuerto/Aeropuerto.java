1892f1581da38adcb95e16ea7078e3e44b348b9f
Carlos Contreras <cdca12mx@gmail.com>
2019-05-09 18:50:31 -0600

    Avance proyecto: Añadidas imagenes

--- src/aeropuerto/Aeropuerto.java
+++ src/aeropuerto/Aeropuerto.java
@@ -1,6 +1,10 @@
 package aeropuerto;
 
+import java.awt.Color;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import utils.Rutinas;
 
 /**
  *
@@ -8,7 +12,14 @@ import javax.swing.JFrame;
  */
 public class Aeropuerto extends JFrame {
 
+    private JPanel glassPaneAviones;
+    private JLabel lbBackground;
+    private final String rutaBackground;
+    
+    
+    
     public Aeropuerto() {
+        rutaBackground = "./src/assets/aeropuerto.jpg";
         initComponents();
         setVisible(true);
     }
@@ -17,8 +28,23 @@ public class Aeropuerto extends JFrame {
         setTitle("Aeropuerto");
         setSize(650, 550);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setResizable(false);
+//        setResizable(false);
         setLocationRelativeTo(null);
+        setVisible(true);
+        
+        // GlassPane utilizado para los aviones
+        glassPaneAviones = (JPanel) getGlassPane();
+        glassPaneAviones.setLayout(null);
+        glassPaneAviones.setVisible(true);
+        
+        // Añadir imagen de fondo
+        lbBackground = new JLabel(Rutinas.AjustarImagen(rutaBackground, getContentPane().getWidth(), getContentPane().getHeight()));
+        add(lbBackground);
+        
+        
+        
+        
+        
     }
     
     
