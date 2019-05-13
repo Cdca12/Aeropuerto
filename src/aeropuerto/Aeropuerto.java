d546f4302818ba5689624c4d05a8f3e8bb616ec9
Carlos Contreras <cdca12mx@gmail.com>
2019-05-12 23:52:31 -0600

    Añadida aleatoriedad para aterrizar, no solo en el principio del escenario

--- src/aeropuerto/Aeropuerto.java
+++ src/aeropuerto/Aeropuerto.java
@@ -2,6 +2,8 @@ package aeropuerto;
 
 import java.awt.Color;
 import java.awt.Point;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -19,55 +21,72 @@ public class Aeropuerto extends JFrame {
     private Avion[] aviones;
     private int numeroAviones;
     private Pista pista;
-    
+    private int contadorAterrizajesSinExito;
+    private Integer turnoActual;
+
     public Aeropuerto() {
         rutaBackground = "./src/assets/hangar6.jpg";
-//        numeroAviones = Rutinas.nextInt(3, 5);
-        numeroAviones = 3; // TEST:
+        numeroAviones = Rutinas.nextInt(3, 5);
+//        numeroAviones = 3; // TEST:
+        contadorAterrizajesSinExito = 0;
+        turnoActual = 1;
         pista = new Pista();
         initComponents();
     }
 
     public void initComponents() {
         setTitle("Aeropuerto");
-        setSize(850, 500);
+//        setSize(850, 500);
+        setSize(1200, 700);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setResizable(false);
         setLocationRelativeTo(null);
         setVisible(true);
-        
+
         // GlassPane utilizado para los aviones
         glassPaneAviones = (JPanel) getGlassPane();
         glassPaneAviones.setLayout(null);
         glassPaneAviones.setVisible(true);
-        
+
         // Añadir imagen de fondo
         lbBackground = new JLabel(Rutinas.AjustarImagen(rutaBackground, getContentPane().getWidth(), getContentPane().getHeight()));
         add(lbBackground);
-        
+
         generarAviones();
+        iniciarAviones();
         
-        
-        
-        
+        // Resultados
+        boolean banderaHilosVivos = false;
+        while (banderaHilosVivos) {
+            banderaHilosVivos = false;
+            for (int i = 0; i < aviones.length; i++) {
+                if (aviones[i].isAlive()) {
+                    banderaHilosVivos = true;
+                    break;
+                }
+            }
+        }
+        System.out.println("Bueno");
     }
-    
+
     private void generarAviones() {
         aviones = new Avion[numeroAviones];
-        Point posicionVuelo = new Point(10, 5),
-        posicionEstacionado = new Point(620, 220);
-        
+        int posicionVueloX = 10, posicionVueloY = 2, posicionEstacionadoX = getWidth() - 590, posicionEstacionadoY = getHeight() - 200;
         for (int i = 0; i < aviones.length; i++) {
-            aviones[i] = new Avion(pista, posicionVuelo, posicionEstacionado);
+            aviones[i] = new Avion((i + 1), pista, posicionVueloX, posicionVueloY, posicionEstacionadoX, posicionEstacionadoY, this);
             glassPaneAviones.add(aviones[i].imagenAvion);
-            posicionVuelo.y += 60;
-            posicionEstacionado = new Point(posicionEstacionado.x - 30, posicionEstacionado.y - 30);
+            posicionVueloY += 65;
+            posicionEstacionadoX -= 30;
+            posicionEstacionadoY -= 45;
+            
         }
+    }
+
+    public void iniciarAviones() {
         // Inicialización de hilos
         for (int i = 0; i < aviones.length; i++) {
             aviones[i].start();
         }
     }
-    
-    
+
 }
--- src/aeropuerto/Avion.java
+++ src/aeropuerto/Avion.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.Point;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
@@ -15,71 +16,112 @@ import utils.Rutinas;
  */
 public class Avion extends Thread {
 
+    private Aeropuerto frameAeropuerto; // Cuestiones de visibilidad y ajustar dimensiones
+    private int avionID;
     private final String rutaImagen;
     private Pista pista;
     public JLabel imagenAvion;
-    private int velocidad;
-    private Point posicionEstacionado;
+    private int velocidad, velocidadAterrizaje;
+    private int posicionVueloX, posicionVueloY, posicionEstacionadoX, posicionEstacionadoY;
+    private static int turnoActual;
+    private int contadorAterrizajesFallidos;
 
-    public Avion(Pista pista, Point posicionVuelo, Point posicionEstacionado) {
+    public Avion(int avionID, Pista pista, int posicionVueloX, int posicionVueloY, int posicionEstacionadoX, int posicionEstacionadoY, Aeropuerto frameAeropuerto) {
+        this.avionID = avionID;
         this.pista = pista;
-        this.posicionEstacionado = posicionEstacionado;
+        this.posicionVueloX = posicionVueloX;
+        this.posicionVueloY = posicionVueloY;
+        this.posicionEstacionadoX = posicionEstacionadoX;
+        this.posicionEstacionadoY = posicionEstacionadoY;
+        this.turnoActual = 1;
+        this.contadorAterrizajesFallidos = 0;
+        this.frameAeropuerto = frameAeropuerto;
         rutaImagen = "./src/assets/ship.png";
         imagenAvion = new JLabel(Rutinas.AjustarImagen(rutaImagen, 100, 80));
-        imagenAvion.setBounds(posicionVuelo.x, posicionVuelo.y, 100, 80);
-        velocidad = 3;
+        imagenAvion.setBounds(posicionVueloX, posicionVueloY, 100, 80);
+        velocidad = 5; // TEST:
+        velocidadAterrizaje = 3;
     }
 
     @Override
     public void run() {
+        primeraVuelta();
         while (true) {
             avanza();
             retrocede();
             if (pista.estaDisponible) {
                 pista.semaforo.Espera();
                 pista.estaDisponible = false;
+                aterriza(frameAeropuerto.getHeight() - 300); // Intento de aterrizaje
                 pista.semaforo.Libera();
-                aterriza();
-                esperar(100);
-                pista.estaDisponible = true;
+                if (turnoActual != avionID) { // Cuando no es su turno, se regresa
+                    contadorAterrizajesFallidos++;
+                    pista.estaDisponible = true;
+                    regresa();
+                    // DUDA: Al intentar aterrizar también tiene que esperarse de 5 - 10 min (?
+//                    esperar(Rutinas.nextInt(500, 1000)); // Equivalente a 5-10 min 
+                    continue;
+                }
+                // Aterrizaje exitoso
+                aterriza(frameAeropuerto.getHeight() - 140);
+                esperar(300);
                 estacionar();
-                return;
+                turnoActual++;
+                esperar(Rutinas.nextInt(500, 1000)); // Equivalente a 5-10 min 
+                pista.estaDisponible = true;
+                break;
             }
         }
-
     }
 
     private void avanza() {
         imagenAvion.setIcon(Rutinas.AjustarImagen(rutaImagen, imagenAvion.getWidth(), imagenAvion.getHeight()));
-        while (imagenAvion.getX() <= 730) {
+        while (imagenAvion.getX() <= frameAeropuerto.getWidth() - 200) {
             imagenAvion.setBounds(imagenAvion.getX() + velocidad, imagenAvion.getY(), imagenAvion.getWidth(), imagenAvion.getHeight());
             SwingUtilities.updateComponentTreeUI(imagenAvion);
             esperar(10);
+            if (Rutinas.nextInt(100) == 1) {
+                break;
+            }
         }
     }
 
     private void retrocede() {
         imagenAvion.setIcon(Rutinas.AjustarImagen(rutaImagen.substring(0, rutaImagen.length() - 4) + "-rotated.png", imagenAvion.getWidth(), imagenAvion.getHeight()));
-        while (imagenAvion.getX() >= 20) {
+        while (imagenAvion.getX() >= 50) {
             imagenAvion.setBounds(imagenAvion.getX() - velocidad, imagenAvion.getY(), imagenAvion.getWidth(), imagenAvion.getHeight());
+            SwingUtilities.updateComponentTreeUI(imagenAvion);
+            esperar(10);
+            if (Rutinas.nextInt(200) == 1) {
+                break;
+            }
+        }
+    }
+
+    private void aterriza(int valorY) {
+        imagenAvion.setIcon(Rutinas.AjustarImagen(rutaImagen.substring(0, rutaImagen.length() - 4) + "-landing.png", 100, 80));
+        while (imagenAvion.getY() <= valorY) {
+            imagenAvion.setBounds(imagenAvion.getX(), imagenAvion.getY() + velocidadAterrizaje, 85, 80);
             SwingUtilities.updateComponentTreeUI(imagenAvion);
             esperar(10);
         }
     }
 
-    private void aterriza() {
+    private void regresa() {
         imagenAvion.setIcon(Rutinas.AjustarImagen(rutaImagen.substring(0, rutaImagen.length() - 4) + "-landing.png", 100, 80));
-        while (imagenAvion.getY() <= 350) {
-            imagenAvion.setBounds(imagenAvion.getX(), imagenAvion.getY() + 2, 85, 80);
+        while (imagenAvion.getY() >= posicionVueloY) {
+            imagenAvion.setBounds(imagenAvion.getX(), imagenAvion.getY() - velocidadAterrizaje, 85, 80);
             SwingUtilities.updateComponentTreeUI(imagenAvion);
             esperar(10);
         }
+        System.out.println("Salio regresa");
+
     }
-    
+
     private void estacionar() {
         imagenAvion.setIcon(Rutinas.AjustarImagen(rutaImagen.substring(0, rutaImagen.length() - 4) + "-parked.png", 100, 80));
-        imagenAvion.setBounds(posicionEstacionado.x, posicionEstacionado.y, imagenAvion.getWidth(), imagenAvion.getHeight());
-        
+        imagenAvion.setBounds(posicionEstacionadoX, posicionEstacionadoY, imagenAvion.getWidth(), imagenAvion.getHeight());
+
     }
 
     private void esperar(int milisegundos) {
@@ -88,6 +130,23 @@ public class Avion extends Thread {
         } catch (InterruptedException ex) {
             Logger.getLogger(Avion.class.getName()).log(Level.SEVERE, null, ex);
         }
+    }
+
+    private void primeraVuelta() {
+        imagenAvion.setIcon(Rutinas.AjustarImagen(rutaImagen, imagenAvion.getWidth(), imagenAvion.getHeight()));
+        while (imagenAvion.getX() <= frameAeropuerto.getWidth() - 200) {
+            imagenAvion.setBounds(imagenAvion.getX() + velocidad, imagenAvion.getY(), imagenAvion.getWidth(), imagenAvion.getHeight());
+            SwingUtilities.updateComponentTreeUI(imagenAvion);
+            esperar(10);
+        }
+
+        imagenAvion.setIcon(Rutinas.AjustarImagen(rutaImagen.substring(0, rutaImagen.length() - 4) + "-rotated.png", imagenAvion.getWidth(), imagenAvion.getHeight()));
+        while (imagenAvion.getX() >= 50) {
+            imagenAvion.setBounds(imagenAvion.getX() - velocidad, imagenAvion.getY(), imagenAvion.getWidth(), imagenAvion.getHeight());
+            SwingUtilities.updateComponentTreeUI(imagenAvion);
+            esperar(10);
+        }
+
     }
 
 }
